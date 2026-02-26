c0e76d29f0005e19a9be4e47882ec4af69772047
Palak <valtorin8@gmail.com>
2026-02-26 21:37:26 +0530

    Time: 0 ms (100.00%) | Memory: 48.7 MB (70.50%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,22 +1,17 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int k = 0;
-        int count = 1;
 
-        nums[k++] = nums[0]; // always take first element
-
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                count++;
-            } else {
-                count = 1;
-            }
-
-            if (count <= 2) {
-                nums[k++] = nums[i];
+        for (int i = 0; i < nums.length; i++) {
+            // Allow placement if fewer than 2 elements written,
+            // OR current element differs from the one written 2 slots back
+            if (k < 2 || nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
+                k++;
             }
         }
 
         return k;
     }
 }
+

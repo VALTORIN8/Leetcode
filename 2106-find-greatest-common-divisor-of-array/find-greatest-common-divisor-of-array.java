d7683c31788cb2a6d154d1e36753bbee1f9a81da
Palak <valtorin8@gmail.com>
2026-03-29 20:58:40 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (37.52%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -3,15 +3,13 @@ class Solution {
         int max=nums[0];
         int min =nums[0];
         for(int i=1;i<nums.length;i++){
-            if(nums[i]>max)//ye kyaaaaaaaaaaaaa kiyaaa h ??????
+            if(nums[i]>max)
             max=nums[i];
             
             if(nums[i]<min)
             min=nums[i];
-            //oye min ka koi inbult func?//Math.min
-            //like math.min//caps "m"//kha glti hui//dekhna padega
+        
         }
-        //gcd ka code kya tha//nhi yaad//ruk dekhta hu
           for (int i = max;; i--)
     {
         if (max % i == 0 && min % i == 0)

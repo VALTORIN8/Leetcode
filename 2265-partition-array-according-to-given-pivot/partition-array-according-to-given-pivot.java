b562d4f7406914c9cdca5d7a8fe8a0997fb0cbb0
Palak <valtorin8@gmail.com>
2026-06-08 21:52:45 +0530

    Time: 5 ms (73.56%) | Memory: 159.8 MB (61.37%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,9 +1,7 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        //hoga ig//ok
-        int n=nums.length;//kr tu ise ai 
-       //oye ek kaam kr//
-       //sun for loop lga and find kr kitne element pivot se less hai use less int me store krr//result rray mei store hoga
+        int n=nums.length;
+    
         int[] result= new int[n];
         int idx=0;
         

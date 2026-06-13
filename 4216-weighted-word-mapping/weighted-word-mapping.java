5f39d7d1fa64d22859feff2d168f4c449c76caf2
Palak <valtorin8@gmail.com>
2026-06-13 23:24:57 +0530

    Time: 7 ms (12.88%) | Memory: 47 MB (8.30%) - LeetSync

--- 4216-weighted-word-mapping/weighted-word-mapping.java
+++ 4216-weighted-word-mapping/weighted-word-mapping.java
@@ -9,3 +9,25 @@ class Solution {
         return res;
     }
 }
+/* class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+
+        String result = "";
+
+        for (int i = 0; i < words.length; i++) {
+            int sum = 0;
+
+            for (int j = 0; j < words[i].length(); j++) {
+
+                char ch = words[i].charAt(j); // use words[i] and j
+                int index = ch - 'a';
+
+                sum += weights[index];
+            }
+
+            result += (char) ('z' - (sum % 26));
+        }
+
+        return result;
+    }
+} */

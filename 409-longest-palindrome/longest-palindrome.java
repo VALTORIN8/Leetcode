b08cef784299e2939920ce0ef31e0d9b3ba722bf
Palak <valtorin8@gmail.com>
2026-06-25 00:32:05 +0530

    Time: 5 ms (58.73%) | Memory: 43.9 MB (5.21%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        HashSet<Character> set=new HashSet<>();
+        int length=0;
+        for(char c :s.toCharArray()){
+            if(set.contains(c)){
+                length+=2;
+                set.remove(c);
+            }
+            else{
+                set.add(c);
+            }
+        }
+        if(!set.isEmpty()){
+            length+=1;
+        }
+        return length;
+    }
+}

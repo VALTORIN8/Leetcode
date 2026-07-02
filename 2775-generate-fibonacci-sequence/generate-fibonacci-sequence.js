ee141891e8acd9536bc9796bf3734ab806fde348
Palak <valtorin8@gmail.com>
2026-07-02 20:51:07 +0530

    Time: 51 ms (14.13%) | Memory: 53.7 MB (47.51%) - LeetSync

--- 2775-generate-fibonacci-sequence/generate-fibonacci-sequence.js
+++ 2775-generate-fibonacci-sequence/generate-fibonacci-sequence.js
@@ -4,13 +4,11 @@
 var fibGenerator = function*() {
     let current = 0;   // pehla number
     let next = 1;      // dusra number
-    //aise hi krde comment sath baad mei samjhne mei easy hoga 
-    while (true) {//yha  dekh //eg dekh call co// submit kre/?
+    while (true) {
         yield current;   // 🛑 current value bhejo aur ruk jao//idhar how//kuch toh ho rha
         [current, next] = [next, current + next];   // ▶️ resume par values update karo
-    }//mereko lag rha oops use kra h something func bana diya bass //baki ka connection
-};//thing is ye stop kb ho rha
-
+    }
+};
 /**
  * const gen = fibGenerator();
  * gen.next().value; // 0

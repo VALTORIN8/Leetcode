00dc442529fad843e3971a9818f3b28f4e1bc313
Palak <valtorin8@gmail.com>
2026-07-02 21:07:08 +0530

    Time: 50 ms (5.85%) | Memory: 47.1 MB (39.68%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,10 +3,10 @@ class Solution {
         int[] result=new int[2];
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){//j=1?==idk//chl baad mei dekhte h
-                if((nums[i]+nums[j])==target && (i!=j)){//test cases passed
+                if((nums[i]+nums[j])==target){//test cases passed
                     result[0]=i;
                     result[1]=j;// na na index hai ig//ha
-                }
+                }//oye ab o(n2) se kam complexity mei 
             }
         }
         return result;

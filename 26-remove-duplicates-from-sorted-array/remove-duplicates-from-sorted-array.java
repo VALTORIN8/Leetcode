fcbcf476718cb3c9e1a9580708b2a412bc706c04
Palak <valtorin8@gmail.com>
2026-07-06 00:55:15 +0530

    Time: 2 ms (8.00%) | Memory: 46.7 MB (62.34%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int count=0;//ye
-        HashSet<Integer> set = new HashSet<>();//time jyada liya hmm sirf hash set use kre to
+        Set<Integer> set = new HashSet<>();//time jyada liya hmm sirf hash set use kre to
         for(int i=0;i<nums.length;i++){//3ms??
             if(set.contains(nums[i])){//vaise set insert order maintain nhi krta ye linkedseth ye krta h no need 
                 continue;

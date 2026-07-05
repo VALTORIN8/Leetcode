665017758347e8b150a8a12c71668c586e986ea0
Palak <valtorin8@gmail.com>
2026-07-06 00:06:33 +0530

    Time: 1 ms (62.66%) | Memory: 43.1 MB (67.56%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,15 +1,30 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        if(strs==null || strs.length==0){
+            return "";
+        }
+        Arrays.sort(strs);
+        String first=strs[0];
+        String last=strs[strs.length -1];
+        int i=0;
+        while(i<first.length() && i<last.length() && 
+                first.charAt(i)==last.charAt(i)){
+                i++;
+            }
+        //bhul gya //dekhna hoga
+        return first.substring(0,i);
+    }
+    
+}
+/*class Solution {
+    public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0) {
             return "";
         }
-        
         // Sort the array - common prefix will be between first and last strings
         Arrays.sort(strs);
-        
         String first = strs[0];
         String last = strs[strs.length - 1];
-        
         // Find common prefix between first and last strings
         int i = 0;
         while (i < first.length() && i < last.length() && 
@@ -19,4 +34,4 @@ class Solution {
         
         return first.substring(0, i);
     }
-}
+}*/

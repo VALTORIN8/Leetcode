64b94f80361d9d750c267ee09718e91fe55afe13
Palak <valtorin8@gmail.com>
2026-07-09 22:54:56 +0530

    Time: 6 ms (70.24%) | Memory: 46.3 MB (47.80%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,6 +1,5 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        //ab likh isme topic likha h sliding window hash table or staff
        // algo
         HashSet<Character> set = new HashSet<>();
         int n=s.length();
@@ -9,17 +8,17 @@ class Solution {
         int ans=0;
         //2. Right ko loop chalao
         for(int right=0;right<n;right++){
+       // 3. Agar duplicate mile
             while(set.contains(s.charAt(right))){
                 set.remove(s.charAt(left));
+           // left se remove karte jao
                 left++;
             }
+        //4. Character add karo
             set.add(s.charAt(right));
+        //5. ans = max(ans, window length)
             ans=Math.max(ans,right-left+1);
         }
         return ans;
-       // 3. Agar duplicate mile
-           // left se remove karte jao
-        //4. Character add karo
-        //5. ans = max(ans, window length)
     }
 }

b3b7c59c0d6057db128e771e4c246bc9de67e2cf
Palak <valtorin8@gmail.com>
2026-07-13 21:53:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (56.57%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -4,17 +4,19 @@ class Solution {
     public int rob(int[] nums) {
         //tu comment mei notes likh//ok
         int n=nums.length;
-        if(n==0)return 0;
-        if(n==1)return nums[0];
+        if(n==0)return 0;//no house skip
+        if(n==1)return nums[0];//one house usse hi chori
         
-        int prev2=nums[0];
-        int prev1=Math.max(nums[0],nums[1]);
-        
-        for(int i=2;i<n;i++){
+        int prev2=nums[0];//1st element
+        int prev1=Math.max(nums[0],nums[1]);//greatest btw phele 2
+ 
+        for(int i=2;i<n;i++){//this describes choices that either rob or skip 
+        //if nums[i]+prev2 is bigger than prev1 which is sum than we update sum 
             int current=Math.max(nums[i]+prev2,prev1);
-        prev2=prev1;
+        prev2=prev1;//update prev1 and prev2
         prev1=current;
         }
+        //like sliding window
     return prev1;
     }
 }

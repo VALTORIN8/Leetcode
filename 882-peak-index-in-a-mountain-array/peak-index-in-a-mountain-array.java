1f32224c3db92f4c2bbc390a06ba831c5f672607
Palak <valtorin8@gmail.com>
2026-07-14 22:12:25 +0530

    Time: 0 ms (100.00%) | Memory: 80.2 MB (77.94%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,15 +1,21 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int n=arr.length;
-        int arr1[]=arr.clone();//aisa bhi kuch hota h 
-        Arrays.sort(arr1);//ha ig try krte hai//ok
-        int max=arr1[n-1];
-        for(int i=0;i<n;i++){
-            if(arr[i]==max){
-                return i;
+        int low = 0;
+        int high = arr.length - 1;
+        
+        while (low < high) {
+            int mid = low + (high - low) / 2;
             
+            if (arr[mid] < arr[mid + 1]) {
+                // We are on the left side of the peak
+                low = mid + 1;
+            } else {
+                // We are on the peak or to the right of it
+                high = mid;
+            }
         }
-        }
-        return -1;
+        
+        // At the end, low == high, pointing to the peak index
+        return low;
     }
 }

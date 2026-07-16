f6fe631a09ca7e038d48e8868f3ac969c518ed63
Palak <valtorin8@gmail.com>
2026-07-16 23:18:50 +0530

    Time: 1 ms (99.99%) | Memory: 44.9 MB (51.93%) - LeetSync

--- 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.java
+++ 3606-minimum-element-after-replacement-with-digit-sum/minimum-element-after-replacement-with-digit-sum.java
@@ -1,23 +1,19 @@
 class Solution {
     public int minElement(int[] nums) {
-//digit sum ka function bana  ke phir elemnet change?        
-for(int i=0;i<nums.length;i++){
-    //ha
-    int sum=0;
-    while(nums[i]>0){
-        int digit=nums[i]%10;
-        sum+=digit;
-        nums[i]/=10;
-    }
-    nums[i]=sum;
-}
-
         int min = Integer.MAX_VALUE;
-        for(int num:nums){
-    if(num<min){
-        min=num;
-    }
-}
-return min;
+        
+        for (int num : nums) {
+            int sum = 0;
+            int temp = num;
+            while (temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+            // Saath ke saath min check kar lo
+            if (sum < min) {
+                min = sum;
+            }
+        }
+        return min;
     }
 }

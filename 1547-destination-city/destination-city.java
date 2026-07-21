b78455f38964aa17199e766456a301a4b4a463b6
Palak <valtorin8@gmail.com>
2026-07-21 23:53:47 +0530

    Time: 2 ms (99.83%) | Memory: 45.4 MB (5.83%) - LeetSync

--- 1547-destination-city/destination-city.java
+++ 1547-destination-city/destination-city.java
@@ -1,4 +1,4 @@
-class Solution {
+/* class Solution {
     public String destCity(List<List<String>> paths) {
         HashSet<String> in = new HashSet<>();
         HashSet<String> out = new HashSet<>();
@@ -13,5 +13,22 @@ class Solution {
         //set print krwa de return in krke dekh//return in .element how?//return krna hog
         return out.iterator().next();
         //ohh 
+    }//ohh konsa
+}//iska ek simple version mila hai */
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        HashSet<String> out = new HashSet<>();
+
+        for (List<String> path : paths) {
+            out.add(path.get(0));
+        }
+
+        for (List<String> path : paths) {
+            if (!out.contains(path.get(1))) {
+                return path.get(1);
+            }
+        }
+
+        return "";
     }
 }

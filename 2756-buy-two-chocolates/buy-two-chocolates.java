de37efca73a14da64ea59b5f5ee8131077367e98
Palak <valtorin8@gmail.com>
2026-08-10 21:35:39 +0530

    Time: 3 ms (46.01%) | Memory: 46.5 MB (41.80%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+      //sabse kam price wali 2 chocolate leni h?//ek meri or dusari princess's best friend ki ok? ok ji
+       Arrays.sort(prices);
+       int sum=prices[0]+prices[1];
+       int result=0;
+       if(sum<=money){
+           result= money-sum; 
+       }
+       else{
+           result=money; 
+       }
+       return result;       //pgl hai kya leftover return krna hai//oh okhmm //aise
+    }
+}
